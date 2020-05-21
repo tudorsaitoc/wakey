fc61ca6585768222ee348ca9c0dfb51635894d5b
ts <tudor@saitoc.me>
2020-05-20 21:56:33 -0500

    config update

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -55,8 +55,8 @@ export default {
 
   // Card Layout
   cards: {
-    imageDimensions: '9x16', // Options: 1x1, 5x4, 4x3, 3x2, 5x3, 16x9, 2x1, 3x1, 4x5, 3x4, 2x3, 3x5, 9x16
-    theme: 'boxes' // Options: boxes, grid, image-grid
+    imageDimensions: '3x1', // Options: 1x1, 5x4, 4x3, 3x2, 5x3, 16x9, 2x1, 3x1, 4x5, 3x4, 2x3, 3x5, 9x16
+    theme: 'grid' // Options: boxes, grid, image-grid
   },
 
   // Disqus
